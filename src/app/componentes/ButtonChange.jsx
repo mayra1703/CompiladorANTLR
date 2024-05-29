69019732f7fe5bf419a2727b4627cce5412be94c
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-05-29 05:05:59 -0700

    add traduccion CToMoonlightCode

--- src/app/componentes/ButtonChange.jsx
+++ src/app/componentes/ButtonChange.jsx
@@ -9,7 +9,7 @@ const options = [
     {id: 3, name: 'Lenguaje Jasmin'},
 ];
 
-function ButtonChange({SelectionChange, SelectionChange2}) {
+function ButtonChange({SelectionChange, SelectionChange2, handleTraductorJasmin, handleMoonlightToC, handleTraductor}) {
     const [selected, setSelected] = useState(options[0]);
     const [selected2, setSelected2] = useState(options[1]);
     const [query, setQuery] = useState('');
@@ -34,6 +34,19 @@ function ButtonChange({SelectionChange, SelectionChange2}) {
 
         if (SelectionChange2) {
             SelectionChange2(value.name);
+        }
+    };
+
+    const onTranslateClick = () => {
+        console.log("onTranslateClick");
+        if (selected2.name === 'Lenguaje Jasmin') {
+            console.log("Traduccion a Jasmin");
+            handleTraductorJasmin();
+        }
+
+        else if (selected2.name == 'Moonlight Code') {
+            console.log("Traduccion Moonlight");
+            handleTraductor();
         }
     };
 
@@ -83,7 +96,8 @@ function ButtonChange({SelectionChange, SelectionChange2}) {
 
             
             <div className='flex p-1 justify-center items-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4" aria-hidden="true" fill="#fadc71">
+                
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onClick={onTranslateClick} className="w-4 h-4 cursor-pointer" aria-hidden="true" fill="#fadc71">
                     <path d="M32 96l320 0V32c0-12.9 7.8-24.6 19.8-29.6s25.7-2.2 34.9 6.9l96 96c6 6 9.4 14.1 9.4 22.6s-3.4 16.6-9.4 22.6l-96 96c-9.2 9.2-22.9 11.9-34.9 6.9s-19.8-16.6-19.8-29.6V160L32 160c-17.7 0-32-14.3-32-32s14.3-32 32-32zM480 352c17.7 0 32 14.3 32 32s-14.3 32-32 32H160v64c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-96-96c-6-6-9.4-14.1-9.4-22.6s3.4-16.6 9.4-22.6l96-96c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 64H480z"/>
                 </svg>
             </div>
---
+++ src/helper/CustomVisitor3.js
@@ -0,0 +1,177 @@
+import CParser from '@/grammar2/CParser.js';
+import HashMap from 'hashmap';
+import CVisitor from '@/grammar2/CVisitor.js';
+
+// This class defines a complete generic visitor for a parse tree produced by CalculadoraParser.
+
+// En vez de visitas, retornar las palabras cambiadas a C
+
+export default class CustomVisitor3 extends CVisitor {
+
+	constructor() {
+		super();
+		this.translatedCode = "";
+	}
+
+	translateType(type){
+		switch(type){
+			case 'int':
+				return 'relaxint';
+
+			case 'float':
+				return 'skyfloat';
+
+			case 'char':
+				return 'nightchar';
+
+			default:
+				return '';
+		}
+	}
+
+	isElseif(){
+		let state = this.translatedCode
+		let len = this.translatedCode.length
+
+		return 'else' == state[len-4] + state[len-3] + state[len-2] + state[len-1]
+	}
+
+	// Visit a parse tree produced by CParser#file.
+	visitFile(ctx) {
+		this.visitChildren(ctx)
+		return this.translatedCode;
+	  }
+  
+  
+	  // Visit a parse tree produced by CParser#start.
+	  visitStart(ctx) {
+		this.translatedCode += "SerenityClass{\n";
+		this.visit(ctx.block());
+		this.translatedCode += "\n}";
+		return;
+	  }
+  
+  
+	  // Visit a parse tree produced by CParser#block.
+	  visitBlock(ctx) {
+		return this.visitChildren(ctx);
+	  }
+  
+  
+	  // Visit a parse tree produced by CParser#contenido.
+	  visitContenido(ctx) {
+		console.log("visitContenido");
+		
+		return this.visitChildren(ctx);
+	  }
+  
+	  // Visit a parse tree produced by CParser#declaracion.
+	  visitDeclaracion(ctx) {
+		console.log("visitDeclaracion");
+		const TYPE = ctx.TYPE().getText();
+		let ID = ctx.ID() ? ctx.ID().getText() : ctx.id.text;
+
+		if (ctx.IGUAL()) {
+			let VALUE = this.visit(ctx.expr());
+			this.translatedCode += `\n# ${this.translateType(
+				TYPE
+			)} ${ID} = ${VALUE} .\n`;
+		} else {
+			this.translatedCode += `\n# ${this.translateType(TYPE)} ${ID} .\n`;
+		}
+
+		return;
+	  }
+  
+  
+	  // Visit a parse tree produced by CParser#asignacion.
+	  visitAsignacion(ctx) {
+		console.log("visitAsignacion");
+		let ID = ctx.ID() ? ctx.ID().getText() : "";
+		let VALUE = this.visit(ctx.expr());
+		this.translatedCode += `\n-> ${ID} = (${VALUE}) .`;
+
+		return;
+	  }
+
+	  // Visit a parse tree produced by CParser#impresion.
+	  visitImpresion(ctx) {
+		console.log("visitImpresion");
+		let content = this.visit(ctx.expr())
+		this.translatedCode += `\n-> WHISPER (${content}) .`;
+
+		return ctx.getText();
+	  }
+  
+  
+	  // Visit a parse tree produced by CParser#condicional.
+	  visitCondicional(ctx) {
+		this.visit(ctx.ifStatement())
+		if(ctx.elseIfStatement()) this.visit(ctx.elseIfStatement())
+		if(ctx.elseStatement()) this.visit(ctx.elseStatement())
+	  }
+  
+  
+	  // Visit a parse tree produced by CParser#ifStatement.
+	  visitIfStatement(ctx) {
+		console.log("visitIfStatement");
+		let condition = this.visit(ctx.expr());
+		
+		this.translatedCode += `${this.isElseif() ? " CozyCondition" : "\nCozyCondition"}(${condition}){`;
+		this.visit(ctx.contenido())
+		this.translatedCode += `\n}`;
+
+		return
+	  }
+  
+  
+	  // Visit a parse tree produced by CParser#elseIfStatement.
+	  visitElseIfStatement(ctx) {
+		console.log("visitElseIfStatement");
+		this.translatedCode += `\nelse`;
+		this.visit(ctx.ifStatement())
+		return
+	  }
+  
+  
+	  // Visit a parse tree produced by CParser#elseStatement.
+	  visitElseStatement(ctx) {
+		console.log("visitElseStatement");
+		this.translatedCode += `\nDreamElse{`;
+		this.visit(ctx.contenido())
+		this.translatedCode += `\n}`;
+		return
+	  }
+  
+	  
+	  // Visit a parse tree produced by CParser#incremento.
+	  visitIncremento(ctx) {
+		console.log("visitIncremento");
+		const id = ctx.ID().getText();
+
+		this.translatedCode += `\n-> ${id}++`;
+
+		return 
+	  }
+
+	  // Visit a parse tree produced by CParser#whileStatement.
+	  visitWhileStatement(ctx) {
+		console.log('visitWhileStatement');
+		let condition = this.visit(ctx.expr());
+		
+		this.translatedCode += `\nDreamLoop(${condition}){`;
+		this.visit(ctx.contenido())
+		this.translatedCode += `\n}`;
+
+		return
+	  }
+
+	  // Visit a parse tree produced by CParser#expr.
+	  visitExpr(ctx) {
+		let exprText = ctx.getText();
+
+		exprText = exprText.replace(/\btrue\b/g, "peaceful").replace(/\bfalse\b/g, "chaotic");
+		return exprText;
+	  }
+  
+  }
